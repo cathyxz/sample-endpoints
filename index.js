7934c844c6bdb01c0b49883d7e3ae075f4e32375
Cathy Zhu <cathyzhu@google.com>
2017-12-19 17:41:57 -0800

    Add cors

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ var countries = require('./countries.json');
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res, next) {
   res.send('Working');
 });
 
-app.get('/countries', function(req, res) {
+app.get('/countries', function(req, res, next) {
   res.send(countries);
 });
 
-app.get('/search/countries', function(req, res) {
+app.get('/search/countries', function(req, res, next) {
   var filtered = [];
 
   if (req.query.hasOwnProperty('q')) {
